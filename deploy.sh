5534e1afe1a521231dc53f03016e5d060d01ebc5
Atsushi <atsushimiyamoto07@gmail.com>
2021-01-02 16:05:04 -0800

    create deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker build -t atsushiiii/multi-server:latest -t atsushiiii/multi-server:$SHA -f flask-docker-travisci-ks8/Dockerfile.prod ./server
+
+docker push atsushiiii/multi-server:latest
+docker push atsushiiii/multi-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image server-deployment server=atsushiiii/multi-server:$SHA
