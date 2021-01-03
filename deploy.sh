c92ea25b5784cd0ad246a05e15cc2de1925b42e5
Atsushi <atsushimiyamoto07@gmail.com>
2021-01-02 19:50:18 -0800

    modified

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t atsushiiii/multi-server:latest -t atsushiiii/multi-server:$SHA -f flask-docker-travisci-ks8/Dockerfile.prod ./server
+docker build -t atsushiiii/multi-server:latest -t atsushiiii/multi-server:$SHA -f flask-docker-travisci-ks8/Dockerfile.prod .
 
 docker push atsushiiii/multi-server:latest
 docker push atsushiiii/multi-server:$SHA
